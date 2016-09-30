bae8e3c33ef8e91a14c6233e27466fbd53604b88
tsague <tsagueymele@yahoo.fr>
2016-09-30 10:34:04 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log('have fun !');
+
+
+
+$("button").click(function(){
+	
+var Mon_objet = {
+	Prenom :$("#first_name").val(),
+	Nom : $("#last_name").val(),
+	Ville : $("#city").val()
+};
+
+console.log(Mon_objet);
+$("#username").html(Mon_objet.Prenom + " " + Mon_objet.Nom);
+
+
+
+});
