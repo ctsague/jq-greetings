1606fec006955c552e830e864fabf82e8418c5d2
ctsague <tsagueymele@yahoo.fr>
2016-10-07 14:07:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 console.log('have fun !');
 
-
-
 $("button").click(function(){
 	var monObjet = {
 		prenom :$("#first_name").val(),
 		nom : $("#last_name").val(),
 		ville : $("#city").val()
 	};
-
 	console.log(monObjet);
 	$("#username").html(monObjet.prenom + " " + monObjet.nom);
 });
